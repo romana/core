a21b881a8ab9755eeb22835c829712be14f639db
Stas A. Kraev <flashvoid@gmail.com>
2015-11-26 21:35:54 +1300

    Comments firewall_test.go

--- agent/firewall_test.go
+++ agent/firewall_test.go
@@ -12,6 +12,8 @@
 // WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 // License for the specific language governing permissions and limitations
 // under the License.
+
+// Tests for firewall.go
 package agent
 
 // Some comments on use of mocking framework in helpers_test.go.
