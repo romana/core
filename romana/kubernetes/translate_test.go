ad6550882b9e12f4d6ba234f99203cee128f149b
Stas A. Kraev <mail@kraev.me>
2016-11-13 09:51:51 +0000

    Add support to listener for namespace wide policies (2)

--- romana/kubernetes/translate_test.go
+++ romana/kubernetes/translate_test.go
@@ -6,13 +6,13 @@ import (
 	"github.com/romana/core/tenant"
 	"sync"
 	//	"bytes"
-//	"fmt"
+	"fmt"
 	"testing"
 )
 
-const A = `{ "Spec" :{ "podSelector" : { "MatchLabels" : { "Key1" : "Val1", "Key2" : "Val2" } } } }`
+// const A = `{ "Spec" :{ "podSelector" : { "MatchLabels" : { "Key1" : "Val1", "Key2" : "Val2" } } } }`
 
-func TestMakeTarget(t *testing.T) {
+func TestTranslateTarget(t *testing.T) {
 	tg := TranslateGroup{
 		kubePolicy: &KubeObject{
 			Metadata: Metadata{
@@ -76,7 +76,107 @@ func TestMakeTarget(t *testing.T) {
 	for _, testCase := range testCases {
 		tg.kubePolicy.Spec.PodSelector = testCase.PodSelector
 		tg.romanaPolicy = &testCase.RomanaPolicy
-		err := tg.makeTarget(&translator)
+		err := tg.translateTarget(&translator)
+		if err != nil {
+			t.Errorf("%s", err)
+		}
+
+		if !testCase.expected(tg.romanaPolicy) {
+			t.Errorf("Failed to translate romana policy %s", tg.romanaPolicy.Name)
+		}
+	}
+}
+
+func TestMakeNextSource(t *testing.T) {
+	tg := TranslateGroup{
+		kubePolicy: &KubeObject{
+			Metadata: Metadata{
+				Namespace: "default",
+			},
+			Spec: Spec{
+				Ingress: []Ingress{
+					Ingress{},
+				},
+			},
+		},
+		romanaPolicy: &common.Policy{
+			Name: "TestPolicy",
+		},
+		ingressIndex: 0,
+	}
+
+	translator := Translator{
+		tenantsCache: []TenantCacheEntry{
+			TenantCacheEntry{
+				Tenant: tenant.Tenant{
+					Name: "default",
+					ID:   3,
+				},
+				Segments: []tenant.Segment{
+					tenant.Segment{
+						Name: "TestSegment",
+						ID: 2,
+					},
+					tenant.Segment{
+						Name: "AnotherTestSegment",
+						ID: 3,
+					},
+				},
+			},
+		},
+		cacheMu: &sync.Mutex{},
+		segmentLabelName: "role",
+	}
+
+	testCases := []struct {
+		From	[]FromEntry
+		RomanaPolicy common.Policy
+		expected     func(*common.Policy) bool
+	}{
+		{
+			From: []FromEntry{
+				FromEntry{
+					Pods: PodSelector{
+						MatchLabels: map[string]string{},
+					},
+				},
+			},
+			RomanaPolicy: common.Policy{
+				Name: "TestPolicyWithoutSegment",
+			},
+			expected: func(p *common.Policy) bool {
+				return p.Peers[0].TenantID == 3
+			},
+		},{
+			From: []FromEntry{
+				FromEntry{
+					Pods: PodSelector{
+						MatchLabels: map[string]string{
+							"role" : "TestSegment",
+						},
+					},
+				},
+				FromEntry{
+					Pods: PodSelector{
+						MatchLabels: map[string]string{
+							"role" : "AnotherTestSegment",
+						},
+					},
+				},
+			},
+			RomanaPolicy: common.Policy{
+				Name: "TestPolicyWithSegments",
+			},
+			expected: func(p *common.Policy) bool {
+				return p.Peers[0].TenantID == 3 && p.Peers[0].SegmentID == 2 && p.Peers[1].TenantID == 3 && p.Peers[1].SegmentID == 3
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		tg.kubePolicy.Spec.Ingress[tg.ingressIndex].From = testCase.From
+		tg.romanaPolicy = &testCase.RomanaPolicy
+		err := tg.makeNextSource(&translator)
 		if err != nil {
 			t.Errorf("%s", err)
 		}
