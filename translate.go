fe358bbb0d2292523d47072b3c53cba4a4253f6b
Stas A. Kraev <mail@kraev.me>
2016-11-13 22:32:27 +0000

    Add support to listener for namespace wide policies (5)

--- translate.go
+++ translate.go
@@ -47,6 +47,88 @@ type Translator struct {
 	segmentLabelName string
 }
 
+func (t *Translator) Init(client *common.RestClient, segmentLabelName string) {
+	t.cacheMu = &sync.Mutex{}
+	t.restClient = client
+	err := t.updateCache()
+	if err == nil {
+		glog.Infof("Translator cache updated - have %d tenant entries", len(t.tenantsCache))
+	} else {
+		glog.Errorf("Translator cache update failed, %s", err)
+	}
+	t.segmentLabelName = segmentLabelName
+}
+
+func (t Translator) GetClient() *common.RestClient {
+	return t.restClient
+}
+
+// Kube2Romana reserved for future use.
+func (t Translator) Kube2Romana(kubePolicy KubeObject) (common.Policy, error) {
+	return common.Policy{}, nil
+}
+
+// Kube2RomanaBulk attempts to translate a list of kubernetes policies into
+// romana representation, returns a list of translated policies and a list
+// of policies that can't be translated in original format.
+func (t Translator) Kube2RomanaBulk(kubePolicies []KubeObject) ([]common.Policy, []KubeObject, error) {
+	glog.Info("In Kube2RomanaBulk")
+	var returnRomanaPolicy []common.Policy
+	var returnKubePolicy []KubeObject
+
+	err := t.updateCache()
+	if err != nil {
+		return returnRomanaPolicy, returnKubePolicy, TranslatorError{ErrorCacheUpdate, err}
+	}
+
+	for kubePolicyNumber, _ := range kubePolicies {
+		romanaPolicy, err := t.translateNetworkPolicy(&kubePolicies[kubePolicyNumber])
+		if err != nil {
+			glog.Errorf("Error during policy translation %s", err)
+			returnKubePolicy = append(returnKubePolicy, kubePolicies[kubePolicyNumber])
+		} else {
+			returnRomanaPolicy = append(returnRomanaPolicy, romanaPolicy)
+		}
+	}
+
+	return returnRomanaPolicy, returnKubePolicy, nil
+
+}
+
+// translateNetworkPolicy translates a Kubernetes policy into
+// Romana policy (see common.Policy) with the following rules:
+// 1. Kubernetes Namespace corresponds to Romana Tenant
+// 2. If Romana Tenant does not exist it is an error (a tenant should
+//    automatically have been created when the namespace was added)
+func (l *Translator) translateNetworkPolicy(kubePolicy *KubeObject) (common.Policy, error) {
+	policyName := fmt.Sprintf("kube.%s.%s", kubePolicy.Metadata.Namespace, kubePolicy.Metadata.Name)
+	romanaPolicy := &common.Policy{Direction: common.PolicyDirectionIngress, Name: policyName, ExternalID: kubePolicy.Metadata.Uid}
+
+	// Prepare translate group with original kubernetes policy and empty romana policy.
+	translateGroup := &TranslateGroup{kubePolicy, romanaPolicy, TranslateGroupStartIndex}
+
+	// Fill in AppliedTo field of romana policy.
+	err := translateGroup.translateTarget(l)
+	if err != nil {
+		return *translateGroup.romanaPolicy, TranslatorError{ErrorTranslatingPolicyTarget, err}
+	}
+
+	// For each Ingress field in kubernetes policy, create Peer and Rule fields in
+	// romana policy.
+	for {
+		err := translateGroup.translateNextIngress(l)
+		if _, ok := err.(NoMoreIngressEntities); ok {
+			break
+		}
+
+		if err != nil {
+			return *translateGroup.romanaPolicy, TranslatorError{ErrorTranslatingPolicyIngress, err}
+		}
+	}
+
+	return *translateGroup.romanaPolicy, nil
+}
+
 // resolveTenantByName retrieves tenant information from romana.
 func (l *Translator) resolveTenantByName(tenantName string) (*tenant.Tenant, error) {
 	t := &tenant.Tenant{Name: tenantName}
@@ -137,6 +219,116 @@ func (l *Translator) getOrAddSegment(namespace string, kubeSegmentName string) (
 	}
 }
 
+type TenantCacheEntry struct {
+	Tenant   tenant.Tenant
+	Segments []tenant.Segment
+}
+
+func (t Translator) checkTenantInCache(tenantName string) *TenantCacheEntry {
+	t.cacheMu.Lock()
+	defer func() {
+		t.cacheMu.Unlock()
+	}()
+
+	for tn, currentTenant := range t.tenantsCache {
+		if currentTenant.Tenant.Name == tenantName {
+			return &t.tenantsCache[tn]
+		}
+	}
+	return nil
+}
+
+// checkTenantInCache checks if given tenant cache entry has a segment with given name.
+func (t Translator) checkSegmentInCache(cacheEntry *TenantCacheEntry, segmentId string) *tenant.Segment {
+	t.cacheMu.Lock()
+	defer func() {
+		t.cacheMu.Unlock()
+	}()
+
+	for segn, currentSegment := range cacheEntry.Segments {
+		if currentSegment.Name == segmentId {
+			return &cacheEntry.Segments[segn]
+		}
+	}
+	return nil
+}
+
+// updateCache contacts romana Tenant service, lists
+// all resources and loads them into memory.
+func (t *Translator) updateCache() error {
+	glog.Info("In updateCache")
+
+	tenantURL, err := t.restClient.GetServiceUrl("tenant")
+	if err != nil {
+		return TranslatorError{ErrorCacheUpdate, err}
+	}
+
+	tenants := []tenant.Tenant{}
+	err = t.restClient.Get(tenantURL+"/tenants", &tenants)
+	if err != nil {
+		return TranslatorError{ErrorCacheUpdate, err}
+	}
+
+	if t.restClient == nil {
+		glog.Fatal("REST client is nil")
+	}
+
+	// tenants := []tenant.Tenant{}
+	// _ = t.restClient.Find(&tenants, common.FindAll)
+
+	t.cacheMu.Lock()
+	defer func() {
+		glog.Infof("Exiting updateCache with %d tenants", len(t.tenantsCache))
+		t.cacheMu.Unlock()
+	}()
+
+	t.tenantsCache = nil
+	for _, ten := range tenants {
+		segments := []tenant.Segment{}
+		fullUrl := fmt.Sprintf("%s/tenants/%d/segments", tenantURL, ten.ID)
+		err = t.restClient.Get(fullUrl, &segments)
+
+		// ignore 404 error here which means no segments
+		// considered to be a zero segments rather then
+		// an error.
+		if err != nil && !checkHttp404(err) {
+			return TranslatorError{ErrorCacheUpdate, err}
+		}
+
+		t.tenantsCache = append(t.tenantsCache, TenantCacheEntry{ten, segments})
+	}
+	return nil
+}
+
+func checkHttp404(err error) (ret bool) {
+	switch e := err.(type) {
+	case common.HttpError:
+		if e.StatusCode == 404 {
+			ret = true
+		}
+	}
+
+	return
+}
+
+type TranslatorError struct {
+	Code    TranslatorErrorType
+	Details error
+}
+
+func (t TranslatorError) Error() string {
+	return fmt.Sprintf("Translator error code %d, %s", t.Code, t.Details)
+}
+
+type TranslatorErrorType int
+
+const (
+	ErrorCacheUpdate TranslatorErrorType = iota
+	ErrorTenantNotIntCache
+	ErrorTranslatingPolicyTarget
+	ErrorTranslatingPolicyIngress
+)
+
 // TranslateGroup represent a state of translation of kubernetes policy
 // into romana policy.
 type TranslateGroup struct {
@@ -144,6 +336,8 @@ type TranslateGroup struct {
 	romanaPolicy *common.Policy
 	ingressIndex int
 }
+
+const TranslateGroupStartIndex = 0
 
 // translateTarget analizes kubePolicy and fills romanaPolicy.AppliedTo field.
 func (tg *TranslateGroup) translateTarget(translator *Translator) error {
@@ -251,8 +445,17 @@ func (tg *TranslateGroup) translateNextIngress(translator *Translator) error {
 		return NoMoreIngressEntities{}
 	}
 
-	_ = tg.makeNextSource(translator)
-	// err = tg.makeNextRule()
+	// Translate Ingress.From into romanaPolicy.ToPorts.
+	err := tg.makeNextSource(translator)
+	if err != nil {
+		return err
+	}
+
+	// Translate Ingress.Ports into romanaPolicy.Rules.
+	err = tg.makeNextRule(translator)
+	if err != nil {
+		return err
+	}
 
 	tg.ingressIndex++
 
@@ -266,186 +469,3 @@ func (e NoMoreIngressEntities) Error() string {
 	return "Done translating"
 }
 
-// translateNetworkPolicy translates a Kubernetes policy into
-// Romana policy (see common.Policy) with the following rules:
-// 1. Kubernetes Namespace corresponds to Romana Tenant
-// 2. If Romana Tenant does not exist it is an error (a tenant should
-//    automatically have been created when the namespace was added)
-func (l *Translator) translateNetworkPolicy(kubePolicy *KubeObject) (common.Policy, error) {
-	policyName := fmt.Sprintf("kube.%s.%s", kubePolicy.Metadata.Namespace, kubePolicy.Metadata.Name)
-	romanaPolicy := &common.Policy{Direction: common.PolicyDirectionIngress, Name: policyName, ExternalID: kubePolicy.Metadata.Uid}
-	translateGroup := &TranslateGroup{kubePolicy, romanaPolicy, 0}
-
-	_ = translateGroup.translateTarget(l)
-
-	_ = translateGroup.makeNextSource(l)
-
-	for {
-		err := translateGroup.translateNextIngress(l)
-		if _, ok := err.(NoMoreIngressEntities); ok {
-			break
-		}
-
-		if err != nil {
-			return *translateGroup.romanaPolicy, nil
-		}
-	}
-
-	return *translateGroup.romanaPolicy, nil
-}
-
-type TenantCacheEntry struct {
-	Tenant   tenant.Tenant
-	Segments []tenant.Segment
-}
-
-func (t Translator) checkTenantInCache(tenantName string) *TenantCacheEntry {
-	t.cacheMu.Lock()
-	defer func() {
-		t.cacheMu.Unlock()
-	}()
-
-	for tn, currentTenant := range t.tenantsCache {
-		if currentTenant.Tenant.Name == tenantName {
-			return &t.tenantsCache[tn]
-		}
-	}
-	return nil
-}
-
-// Kube2Romana reserved for future use.
-func (t Translator) Kube2Romana(kubePolicy KubeObject) (common.Policy, error) {
-	return common.Policy{}, nil
-}
-
-// Kube2RomanaBulk attempts to translate a list of kubernetes policies into
-// romana representation, returns a list of translated policies and a list
-// of policies that can't be translated in original format.
-func (t Translator) Kube2RomanaBulk(kubePolicies []KubeObject) ([]common.Policy, []KubeObject, error) {
-	glog.Info("In Kube2RomanaBulk")
-	var returnRomanaPolicy []common.Policy
-	var returnKubePolicy []KubeObject
-
-	err := t.updateCache()
-	if err != nil {
-		return returnRomanaPolicy, returnKubePolicy, TranslatorError{ErrorCacheUpdate, err}
-	}
-
-	for kubePolicyNumber, _ := range kubePolicies {
-		romanaPolicy, err := t.translateNetworkPolicy(&kubePolicies[kubePolicyNumber])
-		if err != nil {
-			glog.Errorf("Error during policy translation %s", err)
-			returnKubePolicy = append(returnKubePolicy, kubePolicies[kubePolicyNumber])
-		} else {
-			returnRomanaPolicy = append(returnRomanaPolicy, romanaPolicy)
-		}
-	}
-
-	return returnRomanaPolicy, returnKubePolicy, nil
-
-}
-
-// checkTenantInCache checks if given tenant cache entry has a segment with given name.
-func (t Translator) checkSegmentInCache(cacheEntry *TenantCacheEntry, segmentId string) *tenant.Segment {
-	t.cacheMu.Lock()
-	defer func() {
-		t.cacheMu.Unlock()
-	}()
-
-	for segn, currentSegment := range cacheEntry.Segments {
-		if currentSegment.Name == segmentId {
-			return &cacheEntry.Segments[segn]
-		}
-	}
-	return nil
-}
-
-// updateCache contacts romana Tenant service, lists
-// all resources and loads them into memory.
-func (t *Translator) updateCache() error {
-	glog.Info("In updateCache")
-
-	tenantURL, err := t.restClient.GetServiceUrl("tenant")
-	if err != nil {
-		return TranslatorError{ErrorCacheUpdate, err}
-	}
-
-	tenants := []tenant.Tenant{}
-	err = t.restClient.Get(tenantURL+"/tenants", &tenants)
-	if err != nil {
-		return TranslatorError{ErrorCacheUpdate, err}
-	}
-
-	if t.restClient == nil {
-		glog.Fatal("REST client is nil")
-	}
-
-	// tenants := []tenant.Tenant{}
-	// _ = t.restClient.Find(&tenants, common.FindAll)
-
-	t.cacheMu.Lock()
-	defer func() {
-		glog.Infof("Exiting updateCache with %d tenants", len(t.tenantsCache))
-		t.cacheMu.Unlock()
-	}()
-
-	t.tenantsCache = nil
-	for _, ten := range tenants {
-		segments := []tenant.Segment{}
-		fullUrl := fmt.Sprintf("%s/tenants/%d/segments", tenantURL, ten.ID)
-		err = t.restClient.Get(fullUrl, &segments)
-
-		// ignore 404 error here which means no segments
-		// considered to be a zero segments rather then
-		// an error.
-		if err != nil && !checkHttp404(err) {
-			return TranslatorError{ErrorCacheUpdate, err}
-		}
-
-		t.tenantsCache = append(t.tenantsCache, TenantCacheEntry{ten, segments})
-	}
-	return nil
-}
-
-func checkHttp404(err error) (ret bool) {
-	switch e := err.(type) {
-	case common.HttpError:
-		if e.StatusCode == 404 {
-			ret = true
-		}
-	}
-
-	return
-}
-
-func (t *Translator) Init(client *common.RestClient, segmentLabelName string) {
-	t.cacheMu = &sync.Mutex{}
-	t.restClient = client
-	err := t.updateCache()
-	if err == nil {
-		glog.Infof("Translator cache updated - have %d tenant entries", len(t.tenantsCache))
-	} else {
-		glog.Errorf("Translator cache update failed, %s", err)
-	}
-	t.segmentLabelName = segmentLabelName
-}
-
-func (t Translator) GetClient() *common.RestClient {
-	return t.restClient
-}
-
-type TranslatorError struct {
-	Code    TranslatorErrorType
-	Details error
-}
-
-func (t TranslatorError) Error() string {
-	return fmt.Sprintf("Translator error code %d, %s", t.Code, t.Details)
-}
-
-type TranslatorErrorType int
-
-const (
-	ErrorCacheUpdate TranslatorErrorType = iota
-	ErrorTenantNotIntCache
-)
