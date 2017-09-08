4f34cd9190ad9e92ea18df795ac6cadc59ebcc23
pritesh <pritesh@users.noreply.github.com>
2017-09-08 11:38:39 -0700

    ipam: add test case to test DeallocateIP.

--- common/client/ipam_test.go
+++ common/client/ipam_test.go
@@ -20,6 +20,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net"
+	"strings"
 	"testing"
 
 	"github.com/romana/core/common/api"
@@ -249,6 +250,56 @@ func TestIPReuse(t *testing.T) {
 	}
 	if ip.String() != "10.0.0.0" {
 		t.Fatalf("Expected 10.0.0.0, got %s", ip)
+	}
+}
+
+// TestIPAM_DeallocateIP tests that an IP can be
+// de-allocated using IP Name or Address.
+func TestIPAM_DeallocateIP(t *testing.T) {
+	ipam = initIpam(t, "")
+
+	ip, err := ipam.AllocateIP("1", "host1", "ten1", "seg1")
+	t.Logf("TestIPAM_DeallocateIP: Allocated %s for ten1:seg1", ip)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if ip.String() != "10.0.0.0" {
+		t.Fatalf("TestIPAM_DeallocateIP: Expected 10.0.0.0, got %s", ip)
+	}
+
+	ip, err = ipam.AllocateIP("2", "host1", "ten1", "seg1")
+	t.Logf("TestIPAM_DeallocateIP: Allocated %s for ten1:seg1", ip)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if ip.String() != "10.0.0.1" {
+		t.Fatalf("TestIPAM_DeallocateIP: Expected 10.0.0.1, got %s", ip)
+	}
+
+	// Deallocate first IP using IP Name
+	err = ipam.DeallocateIP("1")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("TestIPAM_DeallocateIP: Sucessfully Deallocated IP for ten1:seg1 using IP Name")
+
+	// Deallocate second IP using IP Address
+	err = ipam.DeallocateIP("10.0.0.1")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log("TestIPAM_DeallocateIP: Sucessfully Deallocated IP for ten1:seg1 using IP Address")
+
+	// Negative test case for test 1 above.
+	err = ipam.DeallocateIP("10.0.0.0")
+	if !strings.Contains(err.Error(), "404 Not Found") {
+		t.Fatalf("Expected '404 Not Found' error, got %s", err)
+	}
+
+	// Negative test case for test 2 above.
+	err = ipam.DeallocateIP("2")
+	if !strings.Contains(err.Error(), "404 Not Found") {
+		t.Fatalf("Expected '404 Not Found' error, got %s", err)
 	}
 }
 
